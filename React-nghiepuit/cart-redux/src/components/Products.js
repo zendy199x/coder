8d0733fc86eb8a52fe3d460537a6c03f6f27b856
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-08-14 15:53:43 +0700

    template cart redux

---
+++ React-nghiepuit/cart-redux/src/components/Products.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Product from './Products';
+
+class Products extends Component {
+    render() {
+        return(
+            <section className="section">
+                <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
+                <div className="row">
+                    <Product/>
+                    <Product/>
+                    <Product/>
+                </div>
+            </section>
+
+        )
+    }
+}
+
+export default Products;
