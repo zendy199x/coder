1e0b4034d391c15d8c97bb9b0184546bc34884ac
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-08-15 15:06:18 +0700

    product container

--- React-nghiepuit/cart-redux/src/components/Products.js
+++ React-nghiepuit/cart-redux/src/components/Products.js
@@ -1,34 +1,16 @@
 import React, { Component } from 'react';
-import Product from './Product';
-import { connect } from 'react-redux';
 
 class Products extends Component {
     render() {
-        var { products } = this.props; //this.props.products
         return(
             <section className="section">
                 <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
                 <div className="row">
-                    { this.showProducts(products) }
+                    { this.props.children }
                 </div>
             </section>
         )
     }
-    showProducts(products) {
-        var result = null;
-        if(products.length > 0) {
-            result = products.map((product, index) => {
-                return <Product key={index} product={product}/>
-            });
-        }
-        return result;
-    }
 }
 
-const mapStateToProps = state => {
-    return {
-        products: state.products
-    }
-}
-
-export default connect(mapStateToProps, null)(Products);
+export default Products;
--- React-nghiepuit/cart-redux/src/containers/ProductsContainer.js
+++ React-nghiepuit/cart-redux/src/containers/ProductsContainer.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Products from './../components/Products';
+import Product from './../components/Product';
 
 class ProductsContainer extends Component {
+
     render() {
         var { products } = this.props; //this.props.products
         return(
-            <Products products={ products }/>
+            <Product>
+                { this.showProducts(products) }
+            </Product>
         );
     }
+    
+    showProducts(products) {
+        var result = null;
+        if(products.length > 0) {
+            result = products.map((product, index) => {
+                return <Product key={index} product={product}/>
+            })
+        }
+        return result;
+    }
 }
+
 
 const mapStateToProps = state => {
     return {
@@ -17,4 +32,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, null)(Products);
+export default connect(mapStateToProps, null)(ProductsContainer);
